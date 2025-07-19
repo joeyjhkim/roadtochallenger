52fba7647bf5de2e3e6c7b94b437fba1a9bfeb2b
Joey Jaehyeok Kim <jayjay.hyeok3@gmail.com>
2025-07-18 22:00:30 -0400

    Initial Commit

---
+++ sololeveler/sololevelerApp.swift
@@ -0,0 +1,17 @@
+//
+//  sololevelerApp.swift
+//  sololeveler
+//
+//  Created by Joey Kim on 7/18/25.
+//
+
+import SwiftUI
+
+@main
+struct sololevelerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sololevelerTests/sololevelerTests.swift
@@ -0,0 +1,17 @@
+//
+//  sololevelerTests.swift
+//  sololevelerTests
+//
+//  Created by Joey Kim on 7/18/25.
+//
+
+import Testing
+@testable import sololeveler
+
+struct sololevelerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
